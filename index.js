5049c16a6737db7a91c3b0afbb80e07005cb229f
bredele <olivier.wietrich@gmail.com>
2014-12-16 20:02:05 -0700

    add basic api

---
+++ index.js
@@ -0,0 +1,35 @@
+
+/**
+ * Expose 'matrix'
+ */
+
+module.exports = Matrix;
+
+
+/**
+ * Matrix constructor.
+ * @api public
+ */
+
+function Matrix() {
+	if(!(this instanceof Matrix)) return new Matrix();
+  //do something
+}
+
+
+Matrix.prototype.row = function() {
+	// body...
+};
+
+
+Matrix.prototype.column = function() {
+	// body...
+};
+
+Matrix.prototype.get = function() {
+	// body...
+};
+
+Matrix.prototype.set = function() {
+	// body...
+};
